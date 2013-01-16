38b47aecd99ef038abee7b7279c61708970ca166
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-01-16 18:01:30 +0000

    add rake task for SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'active_record/model_spaces'
 
+if ENV['SIMPLECOV']
+  require 'simplecov'
+  Kernel.const_get("SimpleCov").start
+end
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
