9f748ca94f8f3c057807f21034d8b416e4d65e73
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-01-17 17:32:25 +0000

    test model include module

---
+++ spec/active_record/model_spaces_spec.rb
@@ -0,0 +1,36 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+module ActiveRecord
+  describe ModelSpaces do
+
+    def create_model_spaces_class
+      klass = Class.new
+      class << klass
+        include ActiveRecord::ModelSpaces
+      end
+      klass
+    end
+
+    it "should pass in_model_space calls to the registry" do
+      klass = create_model_spaces_class
+      ModelSpaces::REGISTRY.should_receive(:register_model).with(klass, :foo_space, {})
+      klass.in_model_space :foo_space
+    end
+
+    it "should pass new_version calls to the registry" do
+      klass = create_model_spaces_class
+      ModelSpaces::REGISTRY.should_receive(:new_version).with(klass).and_return do |k, &proc|
+        proc.call.should == :blah
+      end
+      klass.new_version{:blah}
+    end
+
+    it "should pass updated_version calls to the registry" do
+      klass = create_model_spaces_class
+      ModelSpaces::REGISTRY.should_receive(:updated_version).with(klass).and_return do |k, &proc|
+        proc.call.should == :blah
+      end
+      klass.updated_version{:blah}
+    end
+  end
+end
