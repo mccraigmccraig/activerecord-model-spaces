3c9f9c5ca224ba7b87dbae9ba424d66916b93e8b
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-01-22 17:55:46 +0000

    starting Context specs

--- lib/active_record/model_spaces/context.rb
+++ lib/active_record/model_spaces/context.rb
@@ -1,3 +1,5 @@
+require 'active_record/model_spaces/table_names'
+
 module ActiveRecord
   module ModelSpaces
 
@@ -15,12 +17,13 @@ module ActiveRecord
         @model_space_key = model_space_key
         @persistor = persistor
         @current_model_versions = persistor.read_model_space_model_versions(model_space.name, model_space_key)
+        @working_model_versions = {}
       end
 
       # implements the Model.table_name method
       def table_name(model)
         version = working_model_versions[model] || current_model_versions[model]
-        TableNames.table_name(model, model_space.name, model_space_key, model_space.history_versions(model), version)
+        table_name_from_model_version(model, version)
       end
 
       # base table name
@@ -30,19 +33,19 @@ module ActiveRecord
 
       # current table_name, seen by everyone outside of this context
       def current_table_name(model)
-        TableNames.table_name(model, model_space.name, model_space_key, model_space.history_versions(model), current_model_versions[model])
+        table_name_from_model_version(model, current_model_versions[model])
       end
 
       # table_name which would be seen by this context in or after a new_version/updated_version. always returns a name
       def next_table_name(model)
         current_version = current_model_versions[model]
         next_version = TableNames.next_version(model_space.history_versions(model), current_version)
-        TableNames.table_name(model, model_space.name, model_space_key, model_space.history_versions(model), next_version)
+        table_name_from_model_version(model, next_version)
       end
 
       # table_name of working table, seen by this context in or after new_version/updated_version. null if no new_version/updated_version has been issued/completed
       def working_table_name(model)
-        TableNames.table_name(model, model_space.name, model_space_key, model_space.history_versions(model), working_model_versions[model]) if working_model_versions[model]
+        table_name_from_model_version(model, working_model_versions[model]) if working_model_versions[model]
       end
 
       def new_version(model, copy_old_version=false, &block)
@@ -104,7 +107,7 @@ module ActiveRecord
       private
 
       def table_name_from_model_version(model, version)
-        TableNames.table_name(model, model_space.name, model_space_key, model_space.history_versions(model), version)
+        TableNames.table_name(model_space.name, model_space_key, model, model_space.history_versions(model), version)
       end
     end
   end
--- spec/active_record/model_spaces/context_spec.rb
+++ spec/active_record/model_spaces/context_spec.rb
@@ -18,6 +18,54 @@ module ActiveRecord
           p.should_receive(:read_model_space_model_versions).with('foo_space', 'one').and_return(v)
 
           c = Context.new(ms, 'one', p)
+          c.model_space.should == ms
+          c.model_space_key.should == 'one'
+          c.persistor.should == p
+          c.current_model_versions.should == {"Items"=>1, "Users"=>2}
+          c.working_model_versions.should == {}
+        end
+      end
+
+      def create_context(attrs = {})
+        ms = attrs[:model_space] || double('model-space')
+
+        if attrs[:model_space]
+          ms_name = attrs[:model_space].name
+        else
+          ms_name = double('model-space-name')
+          ms.stub(:name).and_return(ms_name)
+        end
+
+        msk = attrs[:model_space_key] || double('model-space-key')
+        p = attrs[:persistor] || double('persistor')
+
+        cmv = attrs[:current_model_versions] || double('current-model-versions')
+        p.should_receive(:read_model_space_model_versions).with(ms_name, msk).and_return(cmv)
+
+        Context.new(ms, msk, p)
+      end
+
+      describe "base_table_name" do
+        it "should return the base table_name" do
+          ctx = create_context
+
+          m = double('model')
+          TableNames.should_receive(:base_table_name).with(m)
+
+          ctx.base_table_name(m)
+        end
+      end
+
+      describe "table_name" do
+        it "should return the current_model_version based table_name if present" do
+          ctx = create_context(:current_model_versions=>{"Users"=>2})
+
+          m = double('model')
+#
+
+        end
+
+        it "should return the working_model_version based table_name if present" do
 
         end
       end
