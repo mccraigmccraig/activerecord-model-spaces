bd9eaf84d75b960132bfa55bd821d76c4df2395b
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-01-22 15:30:11 +0000

    persistor specs

--- lib/active_record/model_spaces/persistor.rb
+++ lib/active_record/model_spaces/persistor.rb
@@ -10,37 +10,38 @@ module ActiveRecord
       def initialize(connection, table_name)
         @connection = connection
         @table_name = table_name || "model_spaces_tables"
+        create_model_spaces_table(@connection, @table_name)
       end
 
       # list all persisted prefixes for a given model space
-      def list_prefixes(model_space_name)
-        connection.select_rows("select model_space_name from #{table_name}").map{|r| r.first}
+      def list_keys(model_space_name)
+        connection.select_rows("select model_space_key from #{table_name} where model_space_name='#{model_space_name}'").map{|r| r.first}
       end
 
-      # returns a map of {ModelName => version} entries for a given model-space and prefix
-      def read_model_space_model_versions(model_space_name, prefix)
-        connection.select_all("select model_name, version from #{table_name} where model_space_name=#{model_space_name} and prefix=#{prefix}").reduce({}){|h,r| h[r["model_name"]] = r["version"]}
+      # returns a map of {ModelName => version} entries for a given model-space and model_space_key
+      def read_model_space_model_versions(model_space_name, model_space_key)
+        connection.select_all("select model_name, version from #{table_name} where model_space_name='#{model_space_name}' and model_space_key='#{model_space_key}'").reduce({}){|h,r| h[r["model_name"]] = r["version"].to_i ; h}
       end
 
       # update
-      def update_model_space_model_versions(model_space_name, prefix, new_model_versions)
+      def update_model_space_model_versions(model_space_name, model_space_key, new_model_versions)
         ActiveRecord::Base.transaction do
-          old_model_versions = read_model_space_model_versions(model_space_name, prefix)
+          old_model_versions = read_model_space_model_versions(model_space_name, model_space_key)
 
           new_model_versions.map do |model_name, new_version|
             old_version = old_model_versions[model_name]
 
             if old_version && new_version && old_version != new_version && new_version != 0
 
-              connection.execute("update #{table_name} set version=#{new_version} where model_space_name=#{model_space_name} and prefix=#{prefix} and model_name=#{model_name}")
+              connection.execute("update #{table_name} set version=#{new_version} where model_space_name='#{model_space_name}' and model_space_key='#{model_space_key}' and model_name='#{model_name}'")
 
             elsif !old_version && new_version && new_version != 0
 
-              connecion.execute("insert into #{table_name} (model_space_name, prefix, model_name, version) values (#{model_space_name}, #{prefix}, #{model_name}, #{new_version})")
+              connection.execute("insert into #{table_name} (model_space_name, model_space_key, model_name, version) values ('#{model_space_name}', '#{model_space_key}', '#{model_name}', #{new_version})")
 
             elsif old_version && ( !new_version || new_version == 0 )
 
-              connection.execute("delete from #{table_name} where model_space_name=#{model_space_name} and prefix=#{prefix} and model_name=#{model_name}")
+              connection.execute("delete from #{table_name} where model_space_name='#{model_space_name}' and model_space_key='#{model_space_key}' and model_name='#{model_name}'")
             end
           end
           true
--- spec/active_record/model_spaces/persistor_spec.rb
+++ spec/active_record/model_spaces/persistor_spec.rb
@@ -7,6 +7,95 @@ module ActiveRecord
 
     describe Persistor do
 
+      def create_persistor(connection, table_name)
+        Persistor.any_instance.should_receive(:create_model_spaces_table)
+        Persistor.new(connection, table_name)
+      end
+
+      describe "initialize" do
+        it "should initialize with a connection and default table_name" do
+          c = double('connection')
+          p = create_persistor(c, nil)
+          p.connection.should == c
+          p.table_name.should == "model_spaces_tables"
+        end
+
+        it "should initialize with a connection and table_name" do
+          c = double('connection')
+          p = create_persistor(c, "foo_model_spaces_tables")
+          p.connection.should == c
+          p.table_name.should == "foo_model_spaces_tables"
+        end
+      end
+
+      describe "list_keys" do
+        it "should query the connection for a list of prefixes for a model_space" do
+          c = double('connection')
+
+          c.should_receive(:select_rows).with("select model_space_key from ms_tables where model_space_name='blah'").and_return([["foo"], ["bar"]])
+
+          p = create_persistor(c, "ms_tables")
+          p.list_keys('blah').should == ["foo", "bar"]
+        end
+      end
+
+      describe "read_model_space_model_versions" do
+        it "should query the connection for model_space model_versions" do
+          c = double('connection')
+
+          c.should_receive(:select_all).with("select model_name, version from ms_tables where model_space_name='blah' and model_space_key='one'").and_return([{"model_name"=>"Users", "version"=>"1"}, {"model_name"=>"Items", "version"=>"0"}])
+
+          p = create_persistor(c, 'ms_tables')
+          p.read_model_space_model_versions('blah', 'one').should == {"Users"=>1, "Items"=>0}
+        end
+      end
+
+      describe "update_model_space_model_versions" do
+        it "should update model_version" do
+          ActiveRecord::Base.stub(:transaction).and_return {|&block| block.call}
+
+          c = double('connection')
+          c.should_receive(:execute).with("update ms_tables set version=2 where model_space_name='blah' and model_space_key='one' and model_name='Foo'")
+          c.should_receive(:execute).with("insert into ms_tables (model_space_name, model_space_key, model_name, version) values ('blah', 'one', 'Bar', 1)")
+          c.should_receive(:execute).with("delete from ms_tables where model_space_name='blah' and model_space_key='one' and model_name='Baz'")
+
+          p = create_persistor(c, 'ms_tables')
+
+          p.should_receive(:read_model_space_model_versions).with('blah', 'one').and_return("Foo"=>1, "Baz"=>2, "Blah"=>1)
+
+          p.update_model_space_model_versions('blah', 'one', "Foo"=>2, "Bar"=>1, "Baz"=>0, "Blah"=>1)
+        end
+      end
+
+      describe "create_model_spaces_table" do
+        it "should not issue migration statements against the connection if the persistor table exists" do
+          c = double('connection')
+          c.should_receive(:table_exists?).with("foo_table").and_return(true)
+          p = Persistor.new(c, "foo_table")
+        end
+
+        it "should issue migration statements against the connection if the persistor table does not exist" do
+          c = double('connection')
+          c.should_receive(:table_exists?).with("foo_table").and_return(false)
+
+          c.should_receive(:create_table).and_return do |table_name, &block|
+            table_name.should == "foo_table"
+
+            t = double("table")
+            t.should_receive(:string).with(:model_space_name, :null=>false)
+            t.should_receive(:string).with(:model_space_key, :null=>false)
+            t.should_receive(:string).with(:model_name, :null=>false)
+            t.should_receive(:integer).with(:version, :null=>false, :default=>0)
+
+            block.call(t)
+          end
+
+          c.should_receive(:add_index).with("foo_table", [:model_space_name, :prefix, :model_name], :unique=>true)
+
+          p = Persistor.new(c, "foo_table")
+        end
+
+      end
     end
   end
 end
