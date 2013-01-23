de608f5179fe62e81df115e2d08575410f13c545
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-01-23 15:30:50 +0000

    some context specs

--- lib/active_record/model_spaces/context.rb
+++ lib/active_record/model_spaces/context.rb
@@ -1,10 +1,12 @@
 require 'active_record/model_spaces/table_names'
+require 'active_record/model_spaces/util'
 
 module ActiveRecord
   module ModelSpaces
 
     # holds the current and working tables for a ModelSpace
     class Context
+      include Util
 
       attr_reader :model_space
       attr_reader :model_space_key
@@ -14,7 +16,7 @@ module ActiveRecord
 
       def initialize(model_space, model_space_key, persistor)
         @model_space = model_space
-        @model_space_key = model_space_key
+        @model_space_key = model_space_key.to_sym
         @persistor = persistor
         @current_model_versions = persistor.read_model_space_model_versions(model_space.name, model_space_key)
         @working_model_versions = {}
@@ -22,7 +24,7 @@ module ActiveRecord
 
       # implements the Model.table_name method
       def table_name(model)
-        version = working_model_versions[model] || current_model_versions[model]
+        version = get_working_model_version(model) || get_current_model_version(model)
         table_name_from_model_version(model, version)
       end
 
@@ -33,27 +35,27 @@ module ActiveRecord
 
       # current table_name, seen by everyone outside of this context
       def current_table_name(model)
-        table_name_from_model_version(model, current_model_versions[model])
+        table_name_from_model_version(model, get_current_model_version(model))
       end
 
       # table_name which would be seen by this context in or after a new_version/updated_version. always returns a name
       def next_table_name(model)
-        current_version = current_model_versions[model]
+        current_version = get_current_model_version(model)
         next_version = TableNames.next_version(model_space.history_versions(model), current_version)
         table_name_from_model_version(model, next_version)
       end
 
       # table_name of working table, seen by this context in or after new_version/updated_version. null if no new_version/updated_version has been issued/completed
       def working_table_name(model)
-        table_name_from_model_version(model, working_model_versions[model]) if working_model_versions[model]
+        table_name_from_model_version(model, get_working_model_version(model)) if get_working_model_version(model)
       end
 
       def new_version(model, copy_old_version=false, &block)
 
-        if working_model_versions[model]
+        if get_working_model_version(model)
           block.call # nothing to do
         else
-          current_version = current_model_versions[model]
+          current_version = get_current_model_version(model)
           next_version = TableNames.next_version(model_space.history_versions(model), current_version)
 
           if next_version != current_version
@@ -62,10 +64,11 @@ module ActiveRecord
               tm = TableManager.new(model)
               tm.recreate_table(base_table_name(model), next_table_name(model))
               tm.copy_table(current_table_name(model), next_table_name(model)) if copy_old_version
-              working_model_versions[model] = next_version
+              set_working_model_version(model, next_version)
               block.call
+              ok = true
             ensure
-              working_model_versions.delete[model] if !ok
+              delete_working_model_versions(model) if !ok
             end
 
           else
@@ -108,6 +111,22 @@ module ActiveRecord
 
       def table_name_from_model_version(model, version)
         TableNames.table_name(model_space.name, model_space_key, model, model_space.history_versions(model), version)
+      end
+
+      def get_current_model_version(model)
+        self.current_model_versions[name_from_model(model)]
+      end
+
+      def get_working_model_version(model)
+        self.working_model_versions[name_from_model(model)]
+      end
+
+      def set_working_model_version(model, version)
+        self.working_model_versions[name_from_model(model)] = version
+      end
+
+      def delete_working_model_version(model)
+        self.working_model_versions.delete(name_from_model(model))
       end
     end
   end
--- spec/active_record/model_spaces/context_spec.rb
+++ spec/active_record/model_spaces/context_spec.rb
@@ -19,7 +19,7 @@ module ActiveRecord
 
           c = Context.new(ms, 'one', p)
           c.model_space.should == ms
-          c.model_space_key.should == 'one'
+          c.model_space_key.should == :one
           c.persistor.should == p
           c.current_model_versions.should == {"Items"=>1, "Users"=>2}
           c.working_model_versions.should == {}
@@ -36,7 +36,12 @@ module ActiveRecord
           ms.stub(:name).and_return(ms_name)
         end
 
-        msk = attrs[:model_space_key] || double('model-space-key')
+        if attrs[:model_space_key]
+          msk = attrs[:model_space_key]
+        else
+          msk = double('model-space-key')
+          msk.stub(:to_sym).and_return(msk)
+        end
         p = attrs[:persistor] || double('persistor')
 
         cmv = attrs[:current_model_versions] || double('current-model-versions')
@@ -45,30 +50,71 @@ module ActiveRecord
         Context.new(ms, msk, p)
       end
 
+      def create_context_with_one_model(im, attrs={})
+        im.stub(:to_s).and_return("Items")
+        ms = ModelSpace.new(:foo)
+        ms.register_model(im, :history_versions=>2)
+        create_context(attrs.merge(:model_space=>ms, :current_model_versions=>{"Items"=>1}))
+      end
+
       describe "base_table_name" do
         it "should return the base table_name" do
           ctx = create_context
-
           m = double('model')
           TableNames.should_receive(:base_table_name).with(m)
-
           ctx.base_table_name(m)
         end
       end
 
       describe "table_name" do
         it "should return the current_model_version based table_name if present" do
-          ctx = create_context(:current_model_versions=>{"Users"=>2})
-
-          m = double('model')
-#
-
+          im = double('items-model')
+          ctx = create_context_with_one_model(im, :model_space_key=>"one")
+          TableNames.should_receive(:table_name).with(:foo, :one, im, 2, 1)
+          ctx.table_name(im)
         end
 
         it "should return the working_model_version based table_name if present" do
-
+          im = double('items-model')
+          ctx = create_context_with_one_model(im, :model_space_key=>"one")
+          ctx.send(:set_working_model_version, im, 2)
+          TableNames.should_receive(:table_name).with(:foo, :one, im, 2, 2)
+          ctx.table_name(im)
         end
       end
+
+
+
+      describe "table_name_from_model_version" do
+        it "should call the TableNames.table_name method" do
+          im = double('items-model')
+          ctx = create_context_with_one_model(im, :model_space_key=>"one")
+
+          v = double('version')
+          TableNames.should_receive(:table_name).with(:foo, :one, im, 2, v)
+          ctx.send(:table_name_from_model_version, im, v)
+
+          TableNames.rspec_reset
+          ctx.send(:table_name_from_model_version, im, 3).should == "foo__one__items__3"
+          ctx.send(:table_name_from_model_version, im, 0).should == "foo__one__items"
+          ctx.send(:table_name_from_model_version, im, nil).should == "foo__one__items"
+        end
+      end
+
+      describe "get_current_model_version & set_working_model_version & get_working_model_version & delete_working_model_version" do
+        it "should set and get the working model version" do
+          im = double('items-model')
+          ctx = create_context_with_one_model(im, :model_space_key=>"one")
+          ctx.send(:get_current_model_version, im).should == 1
+
+          ctx.send(:get_working_model_version, im).should == nil
+          ctx.send(:set_working_model_version, im, 2)
+          ctx.send(:get_working_model_version, im).should == 2
+          ctx.send(:delete_working_model_version, im)
+          ctx.send(:get_working_model_version, im).should == nil
+        end
+      end
+
     end
   end
 end
