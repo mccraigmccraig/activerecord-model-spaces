a638e41ddea082c78579a9268f52bd9cc55625c2
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-01-24 13:11:37 +0000

    move require around in spec_helper so rcov covers sources

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,13 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'active_record/model_spaces'
 
 if ENV['SIMPLECOV']
   require 'simplecov'
   Kernel.const_get("SimpleCov").start
 end
+
+require 'active_record/model_spaces'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
